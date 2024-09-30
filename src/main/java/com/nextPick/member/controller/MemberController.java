bc38890071f62e7b53c706eec8f8b5047d7bfb05
Tizesin <tlsalswns0o@gmail.com>
2024-09-30 14:15:25 +0900

    feat:#5 member 초기 구현

---
+++ src/main/java/com/nextPick/member/controller/MemberController.java
@@ -0,0 +1,109 @@
+package com.nextPick.member.controller;
+
+import com.nextPick.dto.SingleResponseDto;
+import com.nextPick.member.dto.MemberDto;
+import com.nextPick.member.entity.Member;
+import com.nextPick.member.mapper.MemberMapper;
+import com.nextPick.member.service.MemberService;
+import com.nextPick.utils.UriCreator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@Validated
+@RequestMapping
+@RequiredArgsConstructor
+public class MemberController {
+    private final static String MEMBER_DEFAULT_URL = "/members";
+    private final MemberService service;
+    private final MemberMapper memberMapper;
+//    private final EmailVerificationService emailVerificationService;
+//    private final AuthService authService;
+
+    //이메일 인증코드 전송
+//    @PostMapping("/auth-code")
+//    public ResponseEntity signUpMember(@Valid @RequestBody VerificationRequest verificationRequest) {
+//        emailVerificationService.sendCodeToEmail(verificationRequest.getEmail());
+//        return ResponseEntity.accepted().body("이메일로 인증 코드를 전송했습니다. 인증 코드를 입력하여 회원가입을 완료하세요.");
+//    }
+
+    //인증코드 검증
+//    @PostMapping("/verify-auth-code")
+//    public ResponseEntity verifyEmail(@Valid @RequestBody VerificationRequest verificationRequest) {
+//        String email = verificationRequest.getEmail();
+//        String authCode = verificationRequest.getAuthCode();
+//
+//        boolean isVerified = emailVerificationService.verifyCode(email, authCode);
+//        if (!isVerified) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 코드가 올바르지 않습니다.");
+//        }
+//        return ResponseEntity.ok("이메일 인증이 완료되었습니다. 회원가입을 진행하세요.");
+//    }
+
+    /**
+     * 회원가입 하는 메서드
+     *
+     * @param memberDto 회원가입을 위해 받은 memberDto
+     *
+     * @return 201 : Created  & 409 : Conflict
+     */
+    @PostMapping("/member")
+    public ResponseEntity createMember(@Valid @RequestBody MemberDto.Post memberDto) {
+        Member member = memberMapper.memberPostToMember(memberDto);
+        service.createMember(member);
+        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, member.getMemberId());
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/member")
+    public ResponseEntity getMember() {
+        Member member = new Member();
+        Member findMember = service.findMember(member);
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(memberMapper.memberToResponseDto(findMember)), HttpStatus.OK);
+    }
+
+    @PatchMapping("/members")
+    public ResponseEntity patchMember(@Valid @RequestBody MemberDto.Patch patch) {
+
+        Member member = service.updateMember(patch);
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(memberMapper.memberToResponseDto(member)), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/members")
+    public ResponseEntity deleteMember() {
+        Member member = new Member();
+        service.deleteMember(member);
+//        service.deleteMember(authentication);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/members/verify/email")
+    public ResponseEntity emailDuplicationVerify(@Valid @RequestBody MemberDto.DuplicationEmailCheck duplicationEmailCheck) {
+        return service.dupCheckEmail(duplicationEmailCheck.getEmail()) ?
+                new ResponseEntity<>(HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    @PostMapping("/members/verify/nickname")
+    public ResponseEntity nicknameDuplicationVerify(@Valid @RequestBody MemberDto.DuplicationNicknameCheck duplicationNicknameCheck){
+        return service.dupCheckEmail(duplicationNicknameCheck.getNickName()) ?
+                new ResponseEntity<>(HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+}
---
+++ src/main/java/com/nextPick/member/dto/MemberDto.java
@@ -0,0 +1,93 @@
+package com.nextPick.member.dto;
+
+import com.nextPick.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class MemberDto {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Post {
+        @NotNull(message = "이메일은 필수 항목입니다.")
+        @Email
+        private String email;
+
+        @NotNull(message = "성별은 필수 항목입니다.")
+//        @Pattern(regexp = "[M|F]")
+        private String gender;
+
+        @NotNull(message = "직군선택은 필수 항목입니다.")
+        private Member.memberOccupation occupation;
+
+        @NotNull(message = "비밀번호는 필수 항목입니다.")
+        @Size(min = 8, max = 15, message = "비밀번호는 8자에서 15자 사이여야 합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?`~]{8,15}$",
+                message = "비밀번호는 8자이상 15자 이하의 알파벳, 숫자, 특수문자만 포함할 수 있습니다.")
+        private String password;
+
+        @NotNull(message = "비밀번호를 한번더 입력해주세요.")
+        private String confirmPassword;
+
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,8}$",
+                message = "특수문자 제외 2자이상 8자 이하로 입력해주세요.")
+        private String nickname;
+    }
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Patch {
+        @Setter
+        private long memberId;
+
+        @Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야 합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?`~]+$",
+                message = "비밀번호는 알파벳, 숫자, 특수문자만 포함할 수 있습니다.")
+        private String password;
+
+        private String confirmPassword;
+
+        @Pattern(regexp = "^[a-zA-Z가-힣]+$",
+                message = "숫자와 특수문자는 사용할 수 없습니다. 알파벳과 한글만 입력해 주세요.")
+        private String nickname;
+
+        private Member.memberOccupation occupation;
+
+        private Integer guiltyScore;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class DuplicationEmailCheck{
+        @NotNull
+        @Email
+        private String email;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class DuplicationNicknameCheck{
+        @NotNull
+        @Email
+        private String nickName;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Response{
+        private String name;
+        private String gender;
+        private String email;
+        private String nickname;
+        private Member.memberOccupation occupation;
+    }
+}
