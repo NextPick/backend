9c8705c8afe644d68ee2454c9c90d31ad082b8b8
Tizesin <tlsalswns0o@gmail.com>
2024-10-01 11:07:27 +0900

    feat:#5 security 적용 및 patch삭제 + 버그 수정

--- src/main/java/com/nextPick/member/service/MemberService.java
+++ src/main/java/com/nextPick/member/service/MemberService.java
@@ -6,6 +6,7 @@ import com.nextPick.exception.ExceptionCode;
 import com.nextPick.member.dto.MemberDto;
 import com.nextPick.member.entity.Member;
 import com.nextPick.member.repository.MemberRepository;
+import com.nextPick.utils.ExtractMemberAndVerify;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,7 +19,7 @@ import java.util.List;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService extends ExtractMemberAndVerify {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final CustomAuthorityUtils authorityUtils;
@@ -31,31 +32,27 @@ public class MemberService {
         return memberRepository.save(member);
     }
 
-    public Member findMember(Member member){
-//        Member member = extaracfdkl
-        Member findMember = memberRepository.findByEmail(member.getEmail())
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
-        return findMember;
+    public Member findMember(Object principal){
+        return extractMemberFromPrincipal(principal,memberRepository);
     }
 
-    public Member updateMember(MemberDto.Patch member) {
-        Member findMember = memberRepository.findByEmail(member.getEmail())
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
-        if(!member.getNickname().isEmpty())
-            findMember.setNickname(member.getNickname());
-        if(!member.getOccupation().getStatus().isEmpty())
-            findMember.setOccupation(member.getOccupation());
-        if(member.getGuiltyScore() != null)
-            findMember.setGuiltyScore(member.getGuiltyScore());
-        if(!member.getPassword().isEmpty()){
-            if(member.getConfirmPassword().isEmpty() || !member.getPassword().equals(member.getConfirmPassword()))
-                throw new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
-        }
-        return memberRepository.save(findMember);
-    }
+//    public Member updateMember(MemberDto.Patch member,Object principal) {
+//        Member findMember = extractMemberFromPrincipal(principal,memberRepository);
+//        if(!member.getNickname().isEmpty())
+//            findMember.setNickname(member.getNickname());
+//        if(!member.getOccupation().getStatus().isEmpty())
+//            findMember.setOccupation(member.getOccupation());
+//        if(member.getGuiltyScore() != null)
+//            findMember.setGuiltyScore(member.getGuiltyScore());
+//        if(!member.getPassword().isEmpty()){
+//            if(member.getConfirmPassword().isEmpty() || !member.getPassword().equals(member.getConfirmPassword()))
+//                throw new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
+//        }
+//        return memberRepository.save(findMember);
+//    }
 
-    public void deleteMember(String email) {
-        Member member = memberRepository.findByEmail(email).orElseThrow(()-> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+    public void deleteMember(Object principal) {
+        Member member = extractMemberFromPrincipal(principal, memberRepository);
         member.setStatus(Member.memberStatus.DELETED);
         memberRepository.save(member);
     }
@@ -68,11 +65,6 @@ public class MemberService {
     public boolean dupCheckNickname(String nickname){
         Member member = memberRepository.findByNickname(nickname).orElse(null);
         return member != null;
-    }
-
-    public Member findMemberByEmail(String email) {
-        return memberRepository.findByEmail(email).orElseThrow(()->
-                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
     }
 
 }
